c16dde51e8a1c0336d16bdccbbb3b92a2317e445
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-12-02 08:38:38 +0100

    Add day2

---
+++ 2021/day2part1/p_test.go
@@ -0,0 +1,45 @@
+package p_test
+
+import (
+	"aoc/ax"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart(t *testing.T) {
+	for i, tc := range []struct {
+		fname string
+		want  int
+	}{
+		{"small", 150},
+		{"input", 1524750},
+	} {
+		t.Run(fmt.Sprintf("%+v", i), func(t *testing.T) {
+			lines := ax.MustReadFineLines(tc.fname)
+			require.Equal(t, tc.want, run(lines))
+		})
+	}
+}
+
+var pat = regexp.MustCompile(`^(\w+) (\d+)`)
+
+func run(lines []string) int {
+	var horz, depth int
+	for _, line := range lines {
+		parts := pat.FindStringSubmatch(line)
+		dir := parts[1]
+		val := ax.MustParseInt(parts[2])
+		switch dir {
+		case "forward":
+			horz += val
+		case "down":
+			depth += val
+		case "up":
+			depth -= val
+		}
+	}
+	return depth * horz
+}
---
+++ 2021/day2part2/p_test.go
@@ -0,0 +1,46 @@
+package p_test
+
+import (
+	"aoc/ax"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart(t *testing.T) {
+	for i, tc := range []struct {
+		fname string
+		want  int
+	}{
+		{"small", 900},
+		{"input", 1592426537},
+	} {
+		t.Run(fmt.Sprintf("%+v", i), func(t *testing.T) {
+			lines := ax.MustReadFineLines(tc.fname)
+			require.Equal(t, tc.want, run(lines))
+		})
+	}
+}
+
+var pat = regexp.MustCompile(`^(\w+) (\d+)`)
+
+func run(lines []string) int {
+	var horz, depth, aim int
+	for _, line := range lines {
+		parts := pat.FindStringSubmatch(line)
+		dir := parts[1]
+		val := ax.MustParseInt(parts[2])
+		switch dir {
+		case "forward":
+			horz += val
+			depth += aim * val
+		case "down":
+			aim += val
+		case "up":
+			aim -= val
+		}
+	}
+	return depth * horz
+}
