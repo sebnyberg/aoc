f2fdf2e7d567465e13b35221047c4dd58a42ed19
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-12-06 08:13:36 +0100

    Improve parse function comments

--- ax/parse.go
+++ ax/parse.go
@@ -5,30 +5,24 @@ import (
 	"strconv"
 )
 
+// MustParseInt parses the input string as an int with base-10.
+// If unsuccessful, the error will be logged followed by os.Exit(1).
 func MustParseInt[T constraints.Integer](s string) T {
 	return MustParseIntBase[T](s, 10)
 }
 
-// MustParseInt parses the provided int as a 10, 64-bit integer with the
-// provided base. If parsing is unsuccessful, the error will be logged followed
-// by os.Exit(1).
+// MustParseIntBase parses the input string as an int with the provided base.
+// If unsuccessful, the error will be logged followed by os.Exit(1).
 func MustParseIntBase[T constraints.Integer](s string, base int) T {
 	res, err := strconv.ParseInt(s, 10, 64)
 	Check(err, "parse int")
 	return T(res)
 }
 
-// MustParseFloat parses the provided int as a base-10, 64-bit integer. If parsing
-// is unsuccessful, the error will be logged followed by os.Exit(1).
+// MustParseFloat parses the input string as a float.
+// If unsuccessful, the error will be logged followed by os.Exit(1).
 func MustParseFloat[T constraints.Float](s string) T {
-	return MustParseFloatBase[T](s, 10)
-}
-
-// MustParseFloat parses the provided int as a 10, 64-bit integer with the
-// provided base. If parsing is unsuccessful, the error will be logged followed
-// by os.Exit(1).
-func MustParseFloatBase[T constraints.Float](s string, base int) T {
-	res, err := strconv.ParseFloat(s, 10)
+	res, err := strconv.ParseFloat(s, 64)
 	Check(err, "parse int")
 	return T(res)
 }
