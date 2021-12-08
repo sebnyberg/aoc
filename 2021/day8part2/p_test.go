cd35fd076eadaffdae4ac02b44d285f2077087e7
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-12-08 07:28:27 +0100

    day 8

---
+++ 2021/day8part2/p_test.go
@@ -0,0 +1,96 @@
+package p_test
+
+import (
+	"aoc/ax"
+	"fmt"
+	"math/bits"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart(t *testing.T) {
+	for i, tc := range []struct {
+		fname string
+		want  int
+	}{
+		{"small", 61229},
+		{"input", 1019355},
+	} {
+		t.Run(fmt.Sprintf("%+v", i), func(t *testing.T) {
+			lines := ax.MustReadFineLines(tc.fname)
+			require.Equal(t, tc.want, run(lines))
+		})
+	}
+}
+
+func run(rows []string) int {
+	var sum int
+	for _, row := range rows {
+		sum += rowSum(row)
+	}
+	return sum
+}
+
+func chars(field string) int {
+	var res int
+	for i := range field {
+		res |= 1 << (field[i] - 'a')
+	}
+	return res
+}
+
+func rowSum(row string) int {
+	parts := strings.Split(row, "|")
+	cipherFields := strings.Fields(parts[0])
+
+	var matchChars [10]int
+	for _, field := range cipherFields {
+		switch len(field) {
+		case 2:
+			matchChars[1] = chars(field)
+		case 3:
+			matchChars[7] = chars(field)
+		case 4:
+			matchChars[4] = chars(field)
+		case 7:
+			matchChars[8] = chars(field)
+		}
+	}
+
+	outputFields := strings.Fields(parts[1])
+	var sum int
+	for _, field := range outputFields {
+		contents := chars(field)
+		sum *= 10
+		switch len(field) {
+		case 2:
+			sum += 1
+		case 3:
+			sum += 7
+		case 4:
+			sum += 4
+		case 5:
+			switch {
+			case contents&matchChars[1] == matchChars[1]:
+				sum += 3
+			case bits.OnesCount(uint(contents&matchChars[4])) == 3:
+				sum += 5
+			default:
+				sum += 2
+			}
+		case 6:
+			switch {
+			case contents&matchChars[1] != matchChars[1]:
+				sum += 6
+			case contents&matchChars[4] == matchChars[4]:
+				sum += 9
+			default:
+			}
+		case 7:
+			sum += 8
+		}
+	}
+	return sum
+}
