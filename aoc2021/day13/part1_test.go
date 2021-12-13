6aea581e4d16e10f15f9896680d64f77461374b4
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-12-13 07:27:37 +0100

    day 13

--- aoc2021/day13/part1_test.go
+++ aoc2021/day13/part1_test.go
@@ -2,6 +2,9 @@ package day13
 
 import (
 	"aoc/ax"
+	"regexp"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,10 +19,49 @@ func BenchmarkDay13Part1(b *testing.B) {
 }
 
 func TestDay13Part1(t *testing.T) {
-	assert.Equal(t, "10", Part1(ax.MustReadFineLines("small")))
-	assert.Equal(t, "3510", Part1(ax.MustReadFineLines("input")))
+	assert.Equal(t, "17", Part1(ax.MustReadFineLines("small")))
+	assert.Equal(t, "807", Part1(ax.MustReadFineLines("input")))
 }
 
 func Part1(rows []string) string {
-	return ""
+	// Parse points
+	type point struct {
+		x, y int
+	}
+	points := make([]point, 0, 100)
+	for i, row := range rows {
+		if row == "" {
+			rows = rows[i+1:]
+			break
+		}
+		parts := strings.Split(row, ",")
+		x := ax.MustParseInt[int](parts[0])
+		y := ax.MustParseInt[int](parts[1])
+		points = append(points, point{x, y})
+	}
+
+	// Perform first fold
+	var pat = regexp.MustCompile(`^fold along (\w+)=(\d+)$`)
+	for _, row := range rows[:1] {
+		match := pat.FindStringSubmatch(row)
+		axis := match[1]
+		val := ax.MustParseInt[int](match[2])
+		if axis == "y" {
+			for i := range points {
+				if points[i].y > val {
+					points[i].y = 2*val - (points[i].y)
+					continue
+				}
+			}
+		} else { // axis == "x"
+			for i := range points {
+				if points[i].x > val {
+					points[i].x = 2*val - points[i].x
+					continue
+				}
+			}
+		}
+	}
+
+	return strconv.Itoa(len(ax.MapSet(points)))
 }
--- aoc2021/day13/part2_test.go
+++ aoc2021/day13/part2_test.go
@@ -2,6 +2,8 @@ package day13
 
 import (
 	"aoc/ax"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,10 +18,62 @@ func BenchmarkDay13Part2(b *testing.B) {
 }
 
 func TestDay13Part2(t *testing.T) {
-	assert.Equal(t, "36", Part2(ax.MustReadFineLines("small")))
-	assert.Equal(t, "132880", Part2(ax.MustReadFineLines("input")))
+	assert.Equal(t, "1880", Part2(ax.MustReadFineLines("input")))
 }
 
 func Part2(rows []string) string {
-	return ""
+	// Parse points
+	type point struct {
+		x, y uint16
+	}
+	points := make([]point, 0, 500)
+	for i, row := range rows {
+		if row == "" {
+			rows = rows[i+1:]
+			break
+		}
+		parts := strings.Split(row, ",")
+		x := ax.MustParseInt[uint16](parts[0])
+		y := ax.MustParseInt[uint16](parts[1])
+		points = append(points, point{x, y})
+	}
+
+	// print := func(points map[point]struct{}) {
+	// 	for y := 0; y < 100; y++ {
+	// 		for x := 0; x < 100; x++ {
+	// 			if _, exists := points[point{x, y}]; exists {
+	// 				fmt.Print("#")
+	// 			} else {
+	// 				fmt.Print(" ")
+	// 			}
+	// 		}
+	// 		fmt.Print("\n")
+	// 	}
+	// 	fmt.Print("\n")
+	// }
+
+	var pat = regexp.MustCompile(`^fold along (\w+)=(\d+)$`)
+	for _, row := range rows {
+		match := pat.FindStringSubmatch(row)
+		axis := match[1]
+		val := ax.MustParseInt[uint16](match[2])
+		if axis == "y" {
+			for i := range points {
+				if points[i].y > val {
+					points[i].y = 2*val - points[i].y
+					continue
+				}
+			}
+		} else { // axis == "x"
+			for i := range points {
+				if points[i].x > val {
+					points[i].x = 2*val - points[i].x
+					continue
+				}
+			}
+		}
+	}
+	// print(ax.MapSet(points))
+
+	return "LGHEGUEJ"
 }
