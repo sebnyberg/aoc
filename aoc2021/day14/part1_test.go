a5e01135a75c435c293fe38907958d24a16b77d4
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-12-14 07:26:03 +0100

    Remove code golfing parts and add comments

--- aoc2021/day14/part1_test.go
+++ aoc2021/day14/part1_test.go
@@ -1,30 +1,28 @@
-package day13
+package day14
 
 import (
 	"aoc/ax"
-	"fmt"
 	"math"
 	"regexp"
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var day13part1 int
+
 func BenchmarkDay13Part1(b *testing.B) {
-	var res string
 	for i := 0; i < b.N; i++ {
-		res = Part1(ax.MustReadFineLines("input"))
+		day13part1 = Part1(ax.MustReadFineLines("input"))
 	}
-	_ = res
 }
 
 func TestDay13Part1(t *testing.T) {
-	assert.Equal(t, "1588", Part1(ax.MustReadFineLines("small")))
-	assert.Equal(t, "807", Part1(ax.MustReadFineLines("input")))
+	assert.Equal(t, 1588, Part1(ax.MustReadFineLines("small")))
+	assert.Equal(t, 807, Part1(ax.MustReadFineLines("input")))
 }
 
-func Part1(rows []string) string {
+func Part1(rows []string) int {
 	type listNode struct {
 		val  byte
 		next *listNode
@@ -47,15 +45,6 @@ func Part1(rows []string) string {
 		pairs[[2]byte{pair[0], pair[1]}] = insert[0]
 	}
 
-	// print := func(root *listNode) {
-	// 	cur := root.next
-	// 	for cur != nil {
-	// 		fmt.Print(string(cur.val))
-	// 		cur = cur.next
-	// 	}
-	// 	fmt.Print("\n")
-	// }
-
 	for i := 0; i < 10; i++ {
 		cur := root.next
 		for cur.next != nil {
@@ -66,7 +55,7 @@ func Part1(rows []string) string {
 				cur = n.next
 				continue
 			}
-			panic("no matching pair!!")
+			panic("no matching pair")
 		}
 	}
 	var count [26]int
@@ -77,21 +66,15 @@ func Part1(rows []string) string {
 	}
 
 	var maxCount int
-	var maxChar byte = 'A'
 	minCount := math.MaxInt32
-	var minChar byte = 'A'
-	for i, cnt := range count {
+	for _, cnt := range count {
 		if cnt > maxCount {
-			maxChar = byte(i + 'A')
 			maxCount = cnt
 		}
 		if cnt > 0 && cnt < minCount {
-			minChar = byte(i + 'A')
 			minCount = cnt
 		}
 	}
-	fmt.Println(string(minChar), minCount)
-	fmt.Println(string(maxChar), maxCount)
 
-	return strconv.Itoa(maxCount - minCount)
+	return maxCount - minCount
 }
--- aoc2021/day14/part2_test.go
+++ aoc2021/day14/part2_test.go
@@ -1,31 +1,33 @@
-package day13
+package day14
 
 import (
 	"aoc/ax"
 	"math"
 	"regexp"
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func BenchmarkDay13Part2(b *testing.B) {
-	var res string
+var day13part2res int
+
+func BenchmarkDay14Part2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		res = Part2(ax.MustReadFineLines("input"))
+		day13part2res = Part2(ax.MustReadFineLines("input"))
 	}
-	_ = res
 }
 
-func TestDay13Part2(t *testing.T) {
-	assert.Equal(t, "2188189693529", Part2(ax.MustReadFineLines("small")))
-	assert.Equal(t, "3353146900153", Part2(ax.MustReadFineLines("input")))
+func TestDay14Part2(t *testing.T) {
+	assert.Equal(t, 2188189693529, Part2(ax.MustReadFineLines("small")))
+	assert.Equal(t, 3353146900153, Part2(ax.MustReadFineLines("input")))
 }
 
-func Part2(rows []string) string {
+func Part2(rows []string) int {
+	const maxDepth = 40
+
 	firstRow := rows[0]
 
+	// Parse pairs into map
 	rows = rows[2:]
 	pat := regexp.MustCompile(`^(\w{2}) -> (\w)$`)
 	pairs := make(map[[2]byte]byte, len(rows))
@@ -36,49 +38,63 @@ func Part2(rows []string) string {
 		pairs[[2]byte{pair[0], pair[1]}] = insert[0]
 	}
 
-	const maxDepth = 40
-
-	type memKey struct {
-		pair  [2]byte
-		depth int
-	}
+	// Helper to merge to [26]ints together
 	merge := func(a, b *[26]int) *[26]int {
 		for i, count := range b {
 			a[i] += count
 		}
 		return a
 	}
+
+	// Create memoization map, capturing (pair,depth) -> count
+	type memKey struct {
+		pair  [2]byte
+		depth int
+	}
 	mem := make(map[memKey][26]int)
 
-	var visit func(int, [2]byte) [26]int
-	visit = func(depth int, pair [2]byte) [26]int {
+	// visitPair visits a pair, counting occurrences of characters for that pair
+	// and all levels below that pair in depth
+	var visitPair func(depth int, pair [2]byte) [26]int
+	visitPair = func(depth int, pair [2]byte) [26]int {
 		if depth == maxDepth {
 			return [26]int{}
 		}
+
+		// Use memoized value if possible
 		k := memKey{pair, depth}
 		if v, exists := mem[k]; exists {
 			return v
 		}
+
+		// Otherwise, count occurrences of characters for this level and all levels
+		// below the current.
 		v := pairs[pair]
 		res := [26]int{}
-		leftRes := visit(depth+1, [2]byte{pair[0], v})
+		leftRes := visitPair(depth+1, [2]byte{pair[0], v})
 		res = *merge(&res, &leftRes)
-		rightRes := visit(depth+1, [2]byte{v, pair[1]})
+		rightRes := visitPair(depth+1, [2]byte{v, pair[1]})
 		res = *merge(&res, &rightRes)
 		res[v-'A']++
+
+		// Memoize result and return
 		mem[k] = res
 		return mem[k]
 	}
 
+	// Visit all pairs in first row
 	res := [26]int{}
 	for i := 0; i < len(firstRow)-1; i++ {
-		pairRes := visit(0, [2]byte{firstRow[i], firstRow[i+1]})
+		pairRes := visitPair(0, [2]byte{firstRow[i], firstRow[i+1]})
 		res = *merge(&res, &pairRes)
 	}
+
+	// Don't forget to add the contents of the first row!
 	for i := range firstRow {
 		res[firstRow[i]-'A']++
 	}
 
+	// Find max/min count
 	var maxCount int
 	minCount := math.MaxInt64
 	for _, cnt := range res {
@@ -90,5 +106,5 @@ func Part2(rows []string) string {
 		}
 	}
 
-	return strconv.Itoa(maxCount - minCount)
+	return maxCount - minCount
 }
