6111a6b4b4f26c1101fc3be08a77db1989619b22
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-12-02 09:59:59 +0100

    2016/03+04

--- aoc2016/day03/main.go
+++ aoc2016/day03/main.go
@@ -2,15 +2,59 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/sebnyberg/aoc/ax"
 )
 
 func solve1(inf string) string {
 	var res int
+	for _, l := range ax.MustReadFileLines(inf) {
+		parts := strings.Fields(l)
+		a := ax.Atoi(parts[0])
+		b := ax.Atoi(parts[1])
+		c := ax.Atoi(parts[2])
+		xs := []int{a, b, c}
+		sort.Ints(xs)
+		if xs[0]+xs[1] <= xs[2] {
+			// invalid
+		} else {
+			res++
+		}
+	}
 	return fmt.Sprint(res)
 }
 
 func solve2(inf string) string {
 	var res int
+	lines := ax.MustReadFileLines(inf)
+	valid := func(as, bs, cs string) bool {
+		a := ax.Atoi(as)
+		b := ax.Atoi(bs)
+		c := ax.Atoi(cs)
+		xs := []int{a, b, c}
+		sort.Ints(xs)
+		if xs[0]+xs[1] <= xs[2] {
+			return false
+		} else {
+			return true
+		}
+	}
+	for i := 0; i < len(lines); i += 3 {
+		parts1 := strings.Fields(lines[i])
+		parts2 := strings.Fields(lines[i+1])
+		parts3 := strings.Fields(lines[i+2])
+		if valid(parts1[0], parts2[0], parts3[0]) {
+			res++
+		}
+		if valid(parts1[1], parts2[1], parts3[1]) {
+			res++
+		}
+		if valid(parts1[2], parts2[2], parts3[2]) {
+			res++
+		}
+	}
 	return fmt.Sprint(res)
 }
 
--- aoc2016/day04/main.go
+++ aoc2016/day04/main.go
@@ -2,15 +2,98 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/sebnyberg/aoc/ax"
 )
 
 func solve1(inf string) string {
 	var res int
+	lines := ax.MustReadFileLines(inf)
+	// lines = []string{
+	// 	"aaaaa-bbb-z-y-x-123[abxyz]",
+	// 	"a-b-c-d-e-f-g-h-987[abcde]",
+	// 	"not-a-real-room-404[oarel]",
+	// 	"totally-real-room-200[decoy]",
+	// }
+	for _, l := range lines {
+		parts := strings.Split(l, "-")
+		m := len(parts)
+		letterCounts := make([]int, 26)
+		for i := 0; i < m-1; i++ {
+			for _, ch := range parts[i] {
+				letterCounts[ch-'a']++
+			}
+		}
+		idx := make([]int, 26)
+		for i := range idx {
+			idx[i] = i
+		}
+		sort.Slice(idx, func(i, j int) bool {
+			if letterCounts[idx[i]] == letterCounts[idx[j]] {
+				return idx[i] < idx[j]
+			}
+			return letterCounts[idx[i]] > letterCounts[idx[j]]
+		})
+		parts2 := strings.Split(parts[m-1], "[")
+		checksum := parts2[1][:len(parts2[1])-1]
+		sectorID := ax.Atoi(parts2[0])
+		for i, ch := range checksum {
+			if rune(idx[i]+'a') != ch {
+				// fmt.Println("invalid checksum")
+				goto invalid
+			}
+		}
+		res += sectorID
+	invalid:
+	}
 	return fmt.Sprint(res)
 }
 
 func solve2(inf string) string {
 	var res int
+	lines := ax.MustReadFileLines(inf)
+	for _, l := range lines {
+		parts := strings.Split(l, "-")
+		m := len(parts)
+		letterCounts := make([]int, 26)
+		for i := 0; i < m-1; i++ {
+			for _, ch := range parts[i] {
+				letterCounts[ch-'a']++
+			}
+		}
+		idx := make([]int, 26)
+		for i := range idx {
+			idx[i] = i
+		}
+		sort.Slice(idx, func(i, j int) bool {
+			if letterCounts[idx[i]] == letterCounts[idx[j]] {
+				return idx[i] < idx[j]
+			}
+			return letterCounts[idx[i]] > letterCounts[idx[j]]
+		})
+		lastParts := strings.Split(parts[m-1], "[")
+		checksum := lastParts[1][:len(lastParts[1])-1]
+		sectorID := ax.Atoi(lastParts[0])
+		var ss []string
+		for i, ch := range checksum {
+			if rune(idx[i]+'a') != ch {
+				goto invalid
+			}
+		}
+		for _, part := range parts[:m-1] {
+			v := []byte(part)
+			for j := range part {
+				v[j] = byte((((int(part[j]) - 'a') + sectorID) % 26) + 'a')
+			}
+			ss = append(ss, string(v))
+		}
+		if strings.Join(ss, " ") == "northpole object storage" {
+			return fmt.Sprint(sectorID)
+		}
+	invalid:
+	}
 	return fmt.Sprint(res)
 }
 
