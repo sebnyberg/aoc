eb9852c63f35f5b7bb443b4a4c028af57e1b3217
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-12-02 10:19:26 +0100

    2016/05

--- aoc2016/day05/main.go
+++ aoc2016/day05/main.go
@@ -1,17 +1,57 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"strconv"
 )
 
 func solve1(inf string) string {
-	var res int
-	return fmt.Sprint(res)
+	input := "cxdnnyjw"
+	var count int
+	var res []byte
+	h := md5.New()
+	var buf []byte
+	for x := 0; count < 8; x++ {
+		in := input + strconv.Itoa(x)
+		h.Reset()
+		h.Write([]byte(in))
+		buf = h.Sum(buf[:0])
+		a := hex.EncodeToString(buf)
+		if a[:5] == "00000" {
+			res = append(res, a[5])
+			count++
+		}
+	}
+	return fmt.Sprint(string(res))
 }
 
 func solve2(inf string) string {
-	var res int
-	return fmt.Sprint(res)
+	input := "cxdnnyjw"
+	var count int
+	res := make([]byte, 8)
+	h := md5.New()
+	var buf []byte
+	for x := 0; count < 8; x++ {
+		in := input + strconv.Itoa(x)
+		h.Reset()
+		h.Write([]byte(in))
+		buf = h.Sum(buf[:0])
+		a := hex.EncodeToString(buf)
+		if a[:5] == "00000" {
+			idx := int(a[5] - '0')
+			if idx < 0 || idx > 7 {
+				continue
+			}
+			char := a[6]
+			if res[idx] == 0 {
+				res[idx] = char
+				count++
+			}
+		}
+	}
+	return fmt.Sprint(string(res))
 }
 
 func main() {
