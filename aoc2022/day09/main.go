74f85e8f0cb24f95f06d46833603fea546941469
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-12-09 07:57:29 +0100

    More cleaup on isle 9

--- aoc2022/day09/main.go
+++ aoc2022/day09/main.go
@@ -13,53 +13,29 @@ func solve(inf string, ropelen int) any {
 	for i := 0; i < ropelen; i++ {
 		rope[i] = [2]int{0, 0}
 	}
-	seen := make(map[[2]int]bool)
 	deltas := map[string][2]int{
 		"R": {0, 1},
 		"L": {0, -1},
 		"U": {1, 0},
 		"D": {-1, 0},
 	}
-	diags := [][2]int{{1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+	seen := make(map[[2]int]bool)
 	for _, l := range lines {
 		fs := strings.Fields(l)
 		d := deltas[fs[0]]
 		steps := ax.Atoi(fs[1])
 		for k := 0; k < steps; k++ {
+			// Move head
 			rope[0][0] += d[0]
 			rope[0][1] += d[1]
 			for t := 1; t < ropelen; t++ {
-				dj := rope[t-1][1] - rope[t][1]
-				di := rope[t-1][0] - rope[t][0]
-				if dj == 0 {
-					if di >= 2 {
-						rope[t][0]++
-					} else if di <= -2 {
-						rope[t][0]--
-					}
-					continue
-				}
-				if di == 0 {
-					if dj >= 2 {
-						rope[t][1]++
-					} else if dj <= -2 {
-						rope[t][1]--
-					}
-					continue
-				}
-				if ax.Abs(di) <= 1 && ax.Abs(dj) <= 1 {
-					continue
-				}
-				// Find a diagonal that puts the tail in the right spot
-				for _, diag := range diags {
-					i := rope[t][0] + diag[0]
-					j := rope[t][1] + diag[1]
-					di := rope[t-1][0] - i
-					dj := rope[t-1][1] - j
-					if ax.Abs(di) <= 1 && ax.Abs(dj) <= 1 {
-						rope[t] = [2]int{i, j}
-						break
-					}
+				// If the tail is more than 1 distance away in either direction
+				dy := rope[t-1][0] - rope[t][0]
+				dx := rope[t-1][1] - rope[t][1]
+				if ax.Abs(dx) > 1 || ax.Abs(dy) > 1 {
+					// Adjust it by at most 1 toward the head
+					rope[t][0] += ax.Max(-1, ax.Min(1, dy))
+					rope[t][1] += ax.Max(-1, ax.Min(1, dx))
 				}
 			}
 			seen[rope[ropelen-1]] = true
