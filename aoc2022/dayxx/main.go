41bdf602eaf76119da1d7ec1279c8b999c380de0
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-12-12 06:36:03 +0100

    Update template

--- aoc2022/dayxx/main.go
+++ aoc2022/dayxx/main.go
@@ -3,31 +3,23 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/sebnyberg/aoc/ax"
 )
 
 var pat = regexp.MustCompile(``)
 
-func solve1(inf string) string {
-	var res int
-	for _, s := range ax.MustReadFileLines(inf) {
-		// var a, b, c int
-		// fmt.Sscanf(s, "%d %d %d", &a, &b, &c)
-		// ss := pat.FindAllString(s, -1)
-		// a := ax.Atoi(ss[0])
-		_ = s
+func solve1(inf string) any {
+	lines := ax.MustReadFileLines(inf)
+	for _, l := range lines {
+		fs := strings.Fields(l)
+		_ = fs
 	}
-	return fmt.Sprint(res)
-}
-
-func solve2(inf string) string {
-	var res int
-	return fmt.Sprint(res)
+	return ""
 }
 
 func main() {
 	f := "input"
 	fmt.Printf("Result1:\n%v\n", solve1(f))
-	fmt.Printf("Result2:\n%v\n\n", solve2(f))
 }
