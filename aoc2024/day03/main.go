fecf627655f1395c1a8c467276a3023ebbd238c6
Sebastian Nyberg <seb.nyberg90@gmail.com>
2024-12-05 07:18:51 +0100

    2024/day03: idiomatic update

--- aoc2024/day03/main.go
+++ aoc2024/day03/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"regexp"
-	"sort"
-	"strings"
+	"strconv"
 
 	"github.com/sebnyberg/aoc/ax"
 )
 
 var mulpat = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
-var dopat = regexp.MustCompile(`do\(\)`)
-var dontpat = regexp.MustCompile(`don't\(\)`)
 
 func solve1(inf string) any {
-	lines := ax.MustReadFileLines(inf)
+	f, _ := os.Open(inf)
+	defer f.Close()
+	b, _ := io.ReadAll(f)
+	program := string(b)
 	var res int
-	program := strings.Join(lines, "")
 	for _, s := range mulpat.FindAllStringSubmatch(program, -1) {
 		a := ax.MustParseInt[int](s[1])
 		b := ax.MustParseInt[int](s[2])
@@ -25,54 +26,32 @@ func solve1(inf string) any {
 	return res
 }
 
-const (
-	typMul  = 0
-	typDo   = 1
-	typDont = 2
-)
+var pat = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
 
 func solve2(inf string) any {
-	lines := ax.MustReadFileLines(inf)
+	f, _ := os.Open(inf)
+	defer f.Close()
+	b, _ := io.ReadAll(f)
+	program := string(b)
+	pat, _ = regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+	matches := pat.FindAllStringSubmatch(program, -1)
+	enabled := true
 	var res int
-	program := strings.Join(lines, "")
-	type event struct {
-		typ  int
-		idx  int
-		args interface{}
-	}
-	var events []event
-	doidx := dopat.FindAllStringIndex(program, -1)
-	for _, idxs := range doidx {
-		events = append(events, event{typ: typDo, idx: idxs[0], args: nil})
-	}
-	dontidx := dontpat.FindAllStringIndex(program, -1)
-	for _, idxs := range dontidx {
-		events = append(events, event{typ: typDont, idx: idxs[0], args: nil})
-	}
-	mulidx := mulpat.FindAllStringSubmatchIndex(program, -1)
-	for _, idxs := range mulidx {
-		a := ax.MustParseInt[int](program[idxs[2]:idxs[3]])
-		b := ax.MustParseInt[int](program[idxs[4]:idxs[5]])
-		events = append(events, event{typ: typMul, idx: idxs[0], args: []int{a, b}})
-	}
-	sort.Slice(events, func(i, j int) bool {
-		return events[i].idx < events[j].idx
-	})
-	var off bool
-	for _, e := range events {
-		switch e.typ {
-		case typDo:
-			off = false
-		case typDont:
-			off = true
-		case typMul:
-			if !off {
-				intArgs := e.args.([]int)
-				res += intArgs[0] * intArgs[1]
+	for _, parts := range matches {
+		switch parts[0][:3] {
+		case "do(":
+			enabled = true
+		case "don":
+			enabled = false
+		default:
+			if !enabled {
+				continue
 			}
+			a, _ := strconv.Atoi(parts[1])
+			b, _ := strconv.Atoi(parts[2])
+			res += a * b
 		}
 	}
-
 	return res
 }
 
